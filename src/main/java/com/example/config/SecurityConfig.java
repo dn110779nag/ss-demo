8b9d537d1a4993c698bbef15f28e1435f0394726
Alex <Alex@Alex-PC>
2016-05-28 14:41:14 +0300

    first commit

---
+++ src/main/java/com/example/config/SecurityConfig.java
@@ -0,0 +1,31 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.example.config;
+
+    
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
+import org.springframework.security.config.annotation.web.configuration.*;
+
+@EnableWebSecurity
+@EnableGlobalMethodSecurity(prePostEnabled=true)
+public class SecurityConfig {
+
+    @Autowired
+    public void configureGlobal(AuthenticationManagerBuilder auth) throws Exception {
+        auth
+            .inMemoryAuthentication()
+                .withUser("user").password("password").roles("USER")
+                .and()
+                .withUser("admin").password("admin").roles("USER", "ADMIN")
+//                .and()
+//                .and()
+//                .
+                ;
+    }
+}
+
---
+++ src/main/java/com/example/controllers/InndexController.java
@@ -0,0 +1,25 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.example.controllers;
+
+import java.security.Principal;
+import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ *
+ * @author Alex
+ */
+@RestController
+public class InndexController {
+    
+    @RequestMapping("/")
+    @PreAuthorize("hasRole('ADMIN')")
+    public String getIndex(Principal p){
+        return "Hello, "+p.getName();
+    }
+}
